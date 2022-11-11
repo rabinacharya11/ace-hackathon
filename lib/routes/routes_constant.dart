0a86e4b67e55005f93a33327052c41caa3bc1773
Subarna Poudel <subarnapdl@gmail.com>
2022-11-12 00:26:25 +0545

    fixed textbox issues in deliver_screen

--- lib/routes/routes_constant.dart
+++ lib/routes/routes_constant.dart
@@ -2,4 +2,5 @@ class RouteConstant {
   static const splashScreen = '/';
   static const homeScreen = '/home';
   static const clothDetail = '/clothdetail';
+  static const deliverScreen = '/deliverScreen';
 }
