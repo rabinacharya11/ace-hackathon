10251b4f6b6e47d110673200b89e341bcda9fd1f
Rabin Acharya <charms2074@gmail.com>
2022-11-11 22:17:02 +0545

    cloth model created

--- lib/DB/Models/clothModel.dart
+++ lib/DB/Models/clothModel.dart
@@ -1,12 +1,35 @@
 // ignore_for_file: file_names
 
-class ClothModel {
+// class ClothModel {
+//   final String title;
+//   final double price;
+//   final String number;
+//   final String description;
+//   final String productname;
+//   final String imagelink;
+
+//   ClothModel(this.title, this.price, this.number, this.description, this.productname, this.imagelink);
+
+//   factory ClothModel.fromJson(Map<String, dynamic> json )=> ClothModel(title: "");
+// }
+
+class Cloth {
   final String title;
   final double price;
-  final String number;
-  final String description;
-  final String productname;
-  final String imagelink;
+  final String contactNo;
+  final String? description;
+  final String imgUrl;
 
-  ClothModel(this.title, this.price, this.number, this.description, this.productname, this.imagelink);
+  Cloth(
+      {required this.title,
+      required this.price,
+      required this.contactNo,
+      this.description,
+      required this.imgUrl});
+
+  factory Cloth.fromJson(Map<String, dynamic> json) => Cloth(
+      title: json['title'],
+      price: json['price'],
+      contactNo: json['contact'],
+      imgUrl: json['image_link']);
 }
--- lib/clothes_management_sys/presentation/viewcloth.dart
+++ lib/clothes_management_sys/presentation/viewcloth.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:waste_manage_sys/DB/Models/clothModel.dart';
 
 class ViewCloth extends StatelessWidget {
-  final ClothModel cloth;
+  final Cloth cloth;
   const ViewCloth({super.key, required this.cloth});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height: 250, width: double.infinity, child: Image.network(cloth.imagelink));
+    return SizedBox(height: 250, width: double.infinity, child: Image.network(cloth.imgUrl));
   }
 }
