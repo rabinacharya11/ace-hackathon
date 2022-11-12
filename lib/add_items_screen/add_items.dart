325f90495ddbd2f37e8d8562cdbc2464303aa963
shreeecreation <shreeecreation720#gmail.com>
2022-11-12 10:48:23 +0545

    Add item screen half done

--- lib/add_items_screen/add_items.dart
+++ lib/add_items_screen/add_items.dart
@@ -1,11 +1,44 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:waste_manage_sys/routes/routes_constant.dart';
 import 'package:waste_manage_sys/theme/theme_data.dart';
 import 'package:waste_manage_sys/theme/uiparameters.dart';
 
-class AddItemsScreen extends StatelessWidget {
+class AddItemsScreen extends StatefulWidget {
   const AddItemsScreen({super.key});
+
+  @override
+  State<AddItemsScreen> createState() => _AddItemsScreenState();
+}
+
+class _AddItemsScreenState extends State<AddItemsScreen> {
+  List<String> ngos = ["Any", "Krishna Chaudhary", "John Cena", "Jonney Deep", "Clint Eastwood"];
+  String selectval = "Any";
+  File? _photo;
+  bool isuploaded = false;
+  bool isselling = false;
+  bool isdonating = false;
+  Future imgFromGallery() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+      } else {}
+    });
+  }
+
+  Future imgFromCamera() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+      } else {}
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +47,114 @@ class AddItemsScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0.5,
         iconTheme: const IconThemeData(color: Colors.black),
-        titleTextStyle: const TextStyle(
-            color: Colors.black, fontSize: 20, overflow: TextOverflow.ellipsis),
+        titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20, overflow: TextOverflow.ellipsis),
         title: const Text('Add Items'),
       ),
       backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
         child: Column(
           children: [
+            // Your Cart
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+              margin: const EdgeInsets.only(bottom: 8),
+              color: Colors.grey[50],
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Order',
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      const SizedBox(height: 5),
+                      Card(
+                        child: Container(
+                          padding: const EdgeInsets.all(20),
+                          width: UIParameters.getWidth(context) - 28,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      _photo != null
+                                          ? Column(
+                                              children: [
+                                                InkWell(
+                                                  onTap: () {
+                                                    _showPicker(context);
+                                                  },
+                                                  child: Container(
+                                                    decoration: BoxDecoration(color: Colors.white38, borderRadius: BorderRadius.circular(10)),
+                                                    child: Image.file(
+                                                      _photo!,
+                                                      width: 80,
+                                                      height: 80,
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          : Column(
+                                              children: [
+                                                const SizedBox(height: 15),
+                                                Card(
+                                                    color: Colors.grey,
+                                                    child: IconButton(
+                                                        onPressed: () {
+                                                          _showPicker(context);
+                                                        },
+                                                        icon: const Icon(Icons.add))),
+                                                const SizedBox(height: 10),
+                                                const Text("Add Image")
+                                              ],
+                                            ),
+                                      const SizedBox(width: 20),
+                                      SizedBox(
+                                        width: MediaQuery.of(context).size.width / 1.8,
+                                        child: TextFormField(
+                                          decoration: const InputDecoration(
+                                            label: Text("Add your product name"),
+                                            labelStyle: TextStyle(fontSize: 15),
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide(color: Colors.black, width: 5.0),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width / 1.25,
+                                    child: TextFormField(
+                                      maxLines: 3,
+                                      decoration: const InputDecoration(
+                                        label: Text("Add your product description"),
+                                        labelStyle: TextStyle(fontSize: 15),
+                                        border: OutlineInputBorder(
+                                          borderSide: BorderSide(color: Colors.black, width: 5.0),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                ],
+              ),
+            ),
+
             // Deliver To
             Container(
               padding: const EdgeInsets.fromLTRB(10, 5, 10, 20),
@@ -35,8 +168,7 @@ class AddItemsScreen extends StatelessWidget {
                     children: [
                       const Text(
                         'Deliver To',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 17),
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
                       ),
                       const Spacer(),
                       InkWell(
@@ -66,8 +198,7 @@ class AddItemsScreen extends StatelessWidget {
                   // Current Location
                   Container(
                     padding: const EdgeInsets.all(10),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: kPrimaryColor)),
+                    decoration: BoxDecoration(border: Border.all(color: kPrimaryColor)),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -113,49 +244,97 @@ class AddItemsScreen extends StatelessWidget {
                 ],
               ),
             ),
+            const SizedBox(height: 20),
 
-            // Receipt
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-              margin: const EdgeInsets.only(bottom: 8),
-              color: Colors.grey[50],
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Receipt',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('subtotal (include VAT, SD)'),
-                      Text('Rs. 420'),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('Delivery fee'),
-                      Text('Rs. 60'),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  const Divider(),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('Total Bill'),
-                      Text('Rs. 480'),
-                    ],
-                  ),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  height: 40,
+                  width: 100,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          isselling = true;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      child: const Text("Sell", style: TextStyle(fontSize: 16))),
+                ),
+                SizedBox(
+                    height: 40,
+                    width: 100,
+                    child:
+                        ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(backgroundColor: Colors.green), child: const Text("Donate")))
+              ],
             ),
-
+            const SizedBox(height: 20),
+            // Receipt
+            isselling
+                ? Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    margin: const EdgeInsets.only(bottom: 8),
+                    color: Colors.grey[50],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Receipt',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text('subtotal (include VAT, SD)'),
+                            Text('Rs. 420'),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text('Delivery fee'),
+                            Text('Rs. 60'),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        const Divider(),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text('Total Bill'),
+                            Text('Rs. 480'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
+            Container(
+                padding: const EdgeInsets.fromLTRB(10, 5, 10, 20),
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                color: Colors.grey[50],
+                child: Column(children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    child: Card(
+                      child: DropdownButton(
+                        value: selectval,
+                        onChanged: (value) {
+                          setState(() {
+                            selectval = value.toString();
+                          });
+                        },
+                        items: ngos.map((itemone) {
+                          return DropdownMenuItem(value: itemone, child: Text(itemone));
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ])),
             // Place Order
             Container(
               padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
@@ -166,8 +345,7 @@ class AddItemsScreen extends StatelessWidget {
                     duration: Duration(milliseconds: 1500),
                   ));
                   await Future.delayed(const Duration(milliseconds: 2500));
-                  Navigator.of(context)
-                      .pushReplacementNamed(RouteConstant.homeScreen);
+                  Navigator.of(context).pushReplacementNamed(RouteConstant.homeScreen);
                 },
                 style: ElevatedButton.styleFrom(backgroundColor: kPrimaryColor),
                 child: const Text(
@@ -180,5 +358,36 @@ class AddItemsScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Gallery'),
+                    tileColor: Colors.white30,
+                    onTap: () {
+                      imgFromGallery();
+                      Navigator.of(context).pop();
+                    }),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  tileColor: Colors.white30,
+                  onTap: () {
+                    imgFromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
